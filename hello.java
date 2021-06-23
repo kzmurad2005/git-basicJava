753296de3c1d0f7134acae398e78293d66be4d67
kzmurad2005 <kzmurad2008@gmail.com>
2021-06-23 08:22:24 +0600

    Added a new method called gressing to hello.java

--- hello.java
+++ hello.java
@@ -2,6 +2,14 @@ public class hello {
 
     public static void main(String[] args) {
         System.out.println("Hello Worlf");
+
+        greeting("Kazi Murad");
+    }
+
+    public static void greeting(String name) {
+
+        System.out.println("Welcome" + name);
+        
     }
 
 }
