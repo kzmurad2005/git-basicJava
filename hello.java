4702d842358c5c2ef7b198409596e679853f9a5e
kzmurad2005 <kzmurad2008@gmail.com>
2021-06-25 08:40:30 +0600

    all method removed and add message method

--- hello.java
+++ hello.java
@@ -3,22 +3,13 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Worlf");
 
-        display();
+       
     }
 
-    public static void display( ) {
+   public static void message(){
 
-
-        System.out.println("Welcome" + name);
-
-        public static int sqr(int x){
-
-            return x*x;
-        }
-
-        System.out.println(Hello Hello);
-
+    System.out.println("Hooray!! Now we are able to contribute on source...");
+   }
         
-    }
-
+   
 }
