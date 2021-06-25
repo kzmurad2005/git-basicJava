6291649b3510bf838c0581607d28074bc0a39486
kzmurad2005 <kzmurad2008@gmail.com>
2021-06-25 07:41:11 +0600

    Change greeting method

--- hello.java
+++ hello.java
@@ -3,12 +3,12 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("Hello Worlf");
 
-        greeting("Kazi Murad");
+        greeting("Git");
     }
 
     public static void greeting(String name) {
 
-        System.out.println("Welcome" + name);
+        System.out.println("Bye Bye" + name);
         
     }
 
